aa12553cf2dd0f7ba165cf5070e6a5e8d06a8d57
Guilherme R.C. <pl4ces.h0lder@gmail.com>
2026-06-16 22:54:32 +0000

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from google.cloud import storage
+from PIL import Image
+from io import BytesIO
 
 # Dados de exemplo
 df = pd.DataFrame({
@@ -10,3 +13,23 @@ df = pd.DataFrame({
 })
 
 st.dataframe(df, use_container_width = True)
+
+# Cliente autenticado
+client = storage.Client.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+
+bucket_nome = "bucket-copa"
+arquivo = "imagens_jogadores/ale_1.jpg"
+
+bucket = client.bucket(bucket_nome)
+blob = bucket.blob(arquivo)
+
+imagem_bytes = blob.download_as_bytes()
+
+imagem = Image.open(BytesIO(imagem_bytes))
+
+st.image(
+    imagem,
+    caption="Imagem do Datalake - Google Cloud Storage"
+)
